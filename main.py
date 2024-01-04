3f909dadab71231fcda27caafa25502ec5b7cef2
Tacce <102242997+Tacce@users.noreply.github.com>
2024-01-04 23:41:18 +0100

    Refactor and minor changes

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import random
 import time
 import matplotlib.pyplot as plt
 
-MAX_POWER_10 = 9
+MAX_POWER_10 = 8
 
 
 def queen_search(queens):
     while True:
-        nd, pd = initialize_diagonal_arrays(len(queens))
-        k = initial_search(queens, nd, pd)
+        neg_diagonal, pos_diagonal = initialize_diagonal_arrays(len(queens))
+        k = initial_search(queens, neg_diagonal, pos_diagonal)
         if len(queens) > 200:
-            final_search(queens, k, nd, pd)
+            final_search(queens, k, neg_diagonal, pos_diagonal)
             break
         else:
-            final_search_reduced(queens, k, nd, pd)
-            if board_collision(queens) == 0:
+            final_search_reduced(queens, k, neg_diagonal, pos_diagonal)
+            if not board_collision(neg_diagonal, pos_diagonal):
                 break
 
 
-def initial_search(queens, nd, pd):
+def initial_search(queens, neg_diagonal, pos_diagonal):
     n = len(queens)
     queens[:] = list(range(n))
     j = 0
@@ -29,48 +29,50 @@ def initial_search(queens, nd, pd):
             break
         m = random.randint(j, n - 1)
         swap(queens, j, m)
-        add_conflict(queens, j, nd, pd)
-        if total_collisions(queens, j, nd, pd) == 0:
+        add_conflict(queens, j, neg_diagonal, pos_diagonal)
+        if total_collisions(queens, j, neg_diagonal, pos_diagonal) == 0:
             j += 1
         else:
-            remove_conflict(queens, j, nd, pd)
+            remove_conflict(queens, j, neg_diagonal, pos_diagonal)
             swap(queens, j, m)
 
     # place queens with possible collisions
     for i in range(j, n):
         m = random.randint(i, n - 1)
         swap(queens, i, m)
-        add_conflict(queens, i, nd, pd)
+        add_conflict(queens, i, neg_diagonal, pos_diagonal)
 
     # return the number of queens with possible collisions
     return n - j
 
 
-def final_search(queens, k, nd, pd):
+def final_search(queens, k, neg_diagonal, pos_diagonal):
     n = len(queens)
     it = 0
     for i in range(n - k, n):
-        if total_collisions(queens, i, nd, pd) > 0:
+        if total_collisions(queens, i, neg_diagonal, pos_diagonal) > 0:
             while it < 7000:
                 j = random.randint(0, n - 1)
-                swap_collision(queens, i, j, nd, pd)
-                b = (total_collisions(queens, i, nd, pd) > 0) or (total_collisions(queens, j, nd, pd) > 0)
+                swap_collision(queens, i, j, neg_diagonal, pos_diagonal)
+                b = (total_collisions(queens, i, neg_diagonal, pos_diagonal) > 0) or\
+                    (total_collisions(queens, j, neg_diagonal, pos_diagonal) > 0)
                 if b:
-                    swap_collision(queens, i, j, nd, pd)
+                    swap_collision(queens, i, j, neg_diagonal, pos_diagonal)
                     it += 1
                 else:
                     break
 
 
-def final_search_reduced(queens, k, nd, pd):
+def final_search_reduced(queens, k, neg_diagonal, pos_diagonal):
     n = len(queens)
     for i in range(n - k, n):
-        if total_collisions(queens, i, nd, pd) > 0:
+        if total_collisions(queens, i, neg_diagonal, pos_diagonal) > 0:
             for j in range(n):
-                swap_collision(queens, i, j, nd, pd)
-                b = (total_collisions(queens, i, nd, pd) > 0) or (total_collisions(queens, j, nd, pd) > 0)
+                swap_collision(queens, i, j, neg_diagonal, pos_diagonal)
+                b = (total_collisions(queens, i, neg_diagonal, pos_diagonal) > 0) or\
+                    (total_collisions(queens, j, neg_diagonal, pos_diagonal) > 0)
                 if b:
-                    swap_collision(queens, i, j, nd, pd)
+                    swap_collision(queens, i, j, neg_diagonal, pos_diagonal)
                 else:
                     break
 
@@ -104,17 +106,16 @@ def add_conflict(queens, i, neg_diagonal, pos_diagonal):
     pos_diagonal[i - queens[i] + len(queens) - 1] += 1
 
 
-def partial_collision(queens, i):
-    return sum(1 for j in range(i) if (i - j) == abs(queens[i] - queens[j]))
-
-
 def total_collisions(queens, i, neg_diagonal, pos_diagonal):
     n = len(queens)
     return neg_diagonal[i + queens[i]] + pos_diagonal[i - queens[i] + n - 1] - 2  # Exclude self-collision
 
 
-def board_collision(queens):
-    return sum(partial_collision(queens, i) for i in range(len(queens)))
+def board_collision(neg_diagonal, pos_diagonal):
+    for i in range(len(neg_diagonal)):
+        if neg_diagonal[i] > 1 or pos_diagonal[i] > 1:
+            return True
+    return False
 
 
 def test():
@@ -135,7 +136,7 @@ def generateTable(ex_time):
     data = [[p, f"{et:.4f}"] for p, et in zip(power, ex_time)]
     ax.table(cellText=data,
              colLabels=["n", "Execution Time (s)"],
-             loc='center', colWidths=[0.3, 0.3])
+             loc='center', colWidths=[0.1, 0.3])
     ax.axis('off')
     fig = plt.gcf()
     plt.title("Exectution Time for Powers of 10")
@@ -160,4 +161,3 @@ if __name__ == '__main__':
                 end = time.perf_counter()
                 print("Soluzione: " + str(queens))
                 print("Tempo di esecuzione: " + str(end - start) + " s")
-                # print(board_collision(queens))
